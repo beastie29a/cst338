170e16b828642a23fa1bf5d9ae2f9d80b28f8c44
Robert Contreras <robertcontreras@gmail.com>
2015-09-07 16:43:35 -0700

    Single main

--- Assignment1.java
+++ Assignment1.java
@@ -3,19 +3,29 @@
    CST 338 - Fall 2015 Session A
    Assignment 1, Part 1 and 2
 */
-/*
+
 // Import the Scanner Class for keyboard input
 import java.util.Scanner;
+// Import DecimalFormat to format numbers
+import java.text.DecimalFormat;
 
 public class Assignment1
 {
+   // Declasre the constants
+   public static final int MIN_HOURS = 12;
+   public static final int MAX_HOURS = 20;
+
    public static void main(String[] args)
    {
+      // Setup variables and Scanner for Part 1
       String fullName;
       int length;
-      
-      // Create a Scanner class
       Scanner keyboard = new Scanner(System.in);
+
+      // Setup DecimalFormat class for Part 2
+      DecimalFormat formattingObject = new DecimalFormat("#0.0");
+      
+      System.out.println("Begin Part 1\n");
 
       // Prompt the user for input then Get input from the user
       System.out.println("Enter your First name followed by your Last name");
@@ -35,30 +45,9 @@ public class Assignment1
       System.out.println("Your Full Name in lower case: "
          + fullName.toLowerCase());
       
-      // Close the keyboard resource
-      if(keyboard != null)
-      {
-         keyboard.close();
-      }
-   }
-}
-*/
+      System.out.println("\nBegin Part 2\n");
 
-import java.util.Scanner;
-import java.text.DecimalFormat;
-
-public class Assignment1
-{
-   // Declasre the constants
-   public static final int MIN_HOURS = 12;
-   public static final int MAX_HOURS = 20;
-
-   public static void main(String[] args)
-   {
-      // Setup Scanner and DecimalFormat classes
-      Scanner keyboard = new Scanner(System.in);
-      DecimalFormat formattingObject = new DecimalFormat("#0.0");
-
+      // Print out information to the screen
       System.out.println("The minimum amount of time you should spend in");
       System.out.println("this class is between " + MIN_HOURS + " and " + 
          MAX_HOURS + " hours.");
@@ -78,10 +67,11 @@ public class Assignment1
    }
 }
 
-/*
-   *************** Part 1 Output ******************** 
+/* *******************************************************
+********* First Run **********
+Begin Part 1
 
-Enter your First and Last name
+Enter your First name followed by your Last name
 Capitalize the first initial of each: 
 Robert
 Contreras
@@ -90,11 +80,33 @@ The length is: 15 Characters
 Your Full Name in upper case: ROBERT CONTRERAS
 Your Full Name in lower case: robert contreras
 
-   *************** Part 2 Output ********************
+Begin Part 2
 
 The minimum amount of time you should spend in
 this class is between 12 and 20 hours.
-Enter the amount of time you spent to three decimal places: 22.321
-22.3
+Enter the amount of time you spent to three decimal places: 
+23.231
+23.2
 
-*/
+********* Second Run **********
+Begin Part 1
+
+Enter your First name followed by your Last name
+Capitalize the first initial of each: 
+George
+Washington
+Your Full Name is: George Washington
+The length is: 16 Characters
+Your Full Name in upper case: GEORGE WASHINGTON
+Your Full Name in lower case: george washington
+
+Begin Part 2
+
+The minimum amount of time you should spend in
+this class is between 12 and 20 hours.
+Enter the amount of time you spent to three decimal places: 
+17.981
+18.0
+
+
+******************************************************** */
