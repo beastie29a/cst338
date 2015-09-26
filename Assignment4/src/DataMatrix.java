f8f81ad467c3d0ac6918938e520cbe33f681ac3b
Robert Contreras <robertcontreras@gmail.com>
2015-09-26 13:25:08 -0700

    First attempt at DataMatrix Class

--- Assignment4/src/DataMatrix.java
+++ Assignment4/src/DataMatrix.java
@@ -1,4 +1,42 @@
-public Class DataMatrix
-   {
 
+
+public class datamatrix implements barcodeio
+{
+
+   public static final char BLACK_CHAR = '*';
+   public static final char WHITE_CHAR = ' ';
+   private BarcodeImage image;
+   private String text;
+   private int actualWidth, actualHeight;
+
+   public DataMatrix()
+   {
+      actualwidth = actualheight = 0;
+      readtext("undefined");
    }
+
+   public DataMatrix(BarcodeImage image)
+   {
+      if (scan(image))
+          System.out.print("Worked!");
+   }
+
+   public DataMatrix(String text)
+   {
+      if (readtext(text))
+         System.out.print("Worked!");
+   }
+
+   public boolean readtext(String text)
+   {
+      this.text = text;
+   }
+
+   public boolean scan(BarcodeImage image)
+   {
+      this.image = super.clone(image);
+      this.actualHeight = 0;
+      this.actualHeight = 0;
+   }
+
+}
