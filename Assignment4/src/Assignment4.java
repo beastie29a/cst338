b44034c872947e59984d06c5d1f3f3f8709398d7
Robert Contreras <robertcontreras@gmail.com>
2015-09-26 09:02:13 -0700

    Initial commit of java files

---
+++ Assignment4/src/Assignment4.java
@@ -0,0 +1,21 @@
+/**
+   CST 338 - Fall 2015 Session A
+   Assignment 4, Optical Barcode Readers and Writers
+   @author Robert Contreras
+   @author Ryan Doherty
+   @author Hyo Lee
+*/
+
+public Class Assignment4
+{
+
+   public static void main(String[] args)
+   {
+
+      Scanner keyboard = new Scanner(System.in);
+      keyboard.close();
+
+      System.out.println();
+   }
+
+}
