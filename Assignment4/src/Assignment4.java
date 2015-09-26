e60c553d535632a74afb5240947b557ce1ff52f2
Robert Contreras <robertcontreras@gmail.com>
2015-09-26 15:40:06 -0700

    Added Sample main() from ilearn

--- Assignment4/src/Assignment4.java
+++ Assignment4/src/Assignment4.java
@@ -14,11 +14,80 @@ public Class Assignment4
 
       Scanner keyboard = new Scanner(System.in);
       keyboard.close();
+      sampleMain();
+
       testBarcodeImage();
 
       System.out.println();
    }
 
+   public static void sampleMain()
+   {
+      String[] sImageIn =
+      {
+      "                                               ",
+      "                                               ",
+      "                                               ",
+      "     * * * * * * * * * * * * * * * * * * * * * ",
+      "     *                                       * ",
+      "     ****** **** ****** ******* ** *** *****   ",
+      "     *     *    ****************************** ",
+      "     * **    * *        **  *    * * *   *     ",
+      "     *   *    *  *****    *   * *   *  **  *** ",
+      "     *  **     * *** **   **  *    **  ***  *  ",
+      "     ***  * **   **  *   ****    *  *  ** * ** ",
+      "     *****  ***  *  * *   ** ** **  *   * *    ",
+      "     ***************************************** ",
+      "                                               ",
+      "                                               ",
+      "                                               "
+
+      };
+
+
+
+      String[] sImageIn_2 =
+      {
+      "                                          ",
+      "                                          ",
+      "* * * * * * * * * * * * * * * * * * *     ",
+      "*                                    *    ",
+      "**** *** **   ***** ****   *********      ",
+      "* ************ ************ **********    ",
+      "** *      *    *  * * *         * *       ",
+      "***   *  *           * **    *      **    ",
+      "* ** * *  *   * * * **  *   ***   ***     ",
+      "* *           **    *****  *   **   **    ",
+      "****  *  * *  * **  ** *   ** *  * *      ",
+      "**************************************    ",
+      "                                          ",
+      "                                          ",
+      "                                          ",
+      "                                          "
+
+      };
+
+      BarcodeImage bc = new BarcodeImage(sImageIn);
+      DataMatrix dm = new DataMatrix(bc);
+
+      // First secret message
+      dm.translateImageToText();
+      dm.displayTextToConsole();
+      dm.displayImageToConsole();
+
+      // second secret message
+      bc = new BarcodeImage(sImageIn_2);
+      dm.scan(bc);
+      dm.translateImageToText();
+      dm.displayTextToConsole();
+      dm.displayImageToConsole();
+
+      // create your own message
+      dm.readText("What a great resume builder this is!");
+      dm.generateImageFromText();
+      dm.displayTextToConsole();
+      dm.displayImageToConsole();
+   }
 
    public static void testBarcodeImage(){
 
