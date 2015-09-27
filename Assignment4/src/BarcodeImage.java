1fe6eb3dfee5b8139eb96f1381a15215f1c7a2c3
Ryan Doherty <rdoherty@csumb.edu>
2015-09-26 19:03:51 -0700

    Added DataMatrix code and fixed isValid bug
    Added DataMatrix code
    fixed isValid bug
    used setPixel() and getPixel()

--- Assignment4/src/BarcodeImage.java
+++ Assignment4/src/BarcodeImage.java
@@ -8,24 +8,27 @@ class BarcodeImage implements Cloneable {
     
     public BarcodeImage(){
         //initialize 2d array - all elements default to false
-        this.image_data = new boolean[MAX_HEIGHT][MAX_WIDTH];
+        image_data = new boolean[MAX_HEIGHT][MAX_WIDTH];
     }
     
     public BarcodeImage(String[] str_data){
         //initialize 2d array - all elements default to false
-        this.image_data = new boolean[MAX_HEIGHT][MAX_WIDTH];
-        //convert 1d string array to 2d boolean array
-        for (int x=0;x<str_data.length;x++){
-            if (str_data[x].equals("*")){
-                this.image_data[x/(MAX_WIDTH)][x%(MAX_WIDTH)]=true;
-            }
+        image_data = new boolean[MAX_HEIGHT][MAX_WIDTH];
+        
+        if (checkSize(str_data)){
+	        //convert 1d string array to 2d boolean array
+	        for (int x=0;x<str_data.length;x++){
+	            if (str_data[x].equals("*")){
+	                setPixel((x/MAX_WIDTH),(x%MAX_WIDTH),true);
+	            }
+	        }
         }
     }
     
     public boolean getPixel(int row, int col){
         
         if (isValid(row, col)){
-            return this.image_data[row][col];
+            return image_data[row][col];
         }
         return false;
         
@@ -33,27 +36,30 @@ class BarcodeImage implements Cloneable {
     
     public boolean setPixel(int row, int col, boolean value){
         if (isValid(row, col)){
-            this.image_data[row][col]=value;
+            image_data[row][col]=value;
             return true;
         }
         return false;
     }
     
     private boolean isValid(int row, int col){
-        if ((row>0 && row<MAX_WIDTH) && (col>0 && col<MAX_HEIGHT)){
+        if ((row>=0 && row<MAX_HEIGHT) && (col>=0 && col<MAX_WIDTH)){
             return true;
         }
         return false;
     }
     
     private boolean checkSize(String[] data){
+    	if (data.length<=MAX_LENGTH && data.length>=0){
+    		return true;
+    	}
         return false;
     }
     
     public void displayToConsole(){
-        for (int x=0;x<this.image_data.length;x++){
-            for (int i=0;i<this.image_data[x].length;i++){
-                if (this.image_data[x][i]){
+        for (int x=0;x<image_data.length;x++){
+            for (int i=0;i<image_data[x].length;i++){
+                if (this.getPixel(x,i)){
                     System.out.print("*");
                 }else{
                     System.out.print(" ");
@@ -65,9 +71,9 @@ class BarcodeImage implements Cloneable {
     
     public BarcodeImage clone(){
         BarcodeImage clonedBarcodeImage = new BarcodeImage();
-        for (int x=0;x<this.image_data.length;x++){
-            for (int i=0;i<this.image_data[x].length;i++){
-                clonedBarcodeImage.image_data[x][i]=this.image_data[x][i];
+        for (int x=0;x<image_data.length;x++){
+            for (int i=0;i<image_data[x].length;i++){
+                clonedBarcodeImage.setPixel(x,i,getPixel(x,i));
             }
         }
         
--- Assignment4/src/DataMatrix.java
+++ Assignment4/src/DataMatrix.java
@@ -1,4 +1,128 @@
-public class DataMatrix
-   {
 
+public class DataMatrix implements BarcodeIO
+{
+
+   public static final char BLACK_CHAR = '*';
+   public static final char WHITE_CHAR = ' ';
+   private BarcodeImage image;
+   private String text;
+   private int actualWidth, actualHeight;
+
+   public DataMatrix()
+   {
+      this.image = new BarcodeImage();
+      actualWidth = actualHeight = 0;
+      readText("undefined");
    }
+
+   public DataMatrix(BarcodeImage image)
+   {
+      if (scan(image))
+          System.out.print("Worked!");
+   }
+
+   public DataMatrix(String text)
+   {
+      if (readText(text))
+         System.out.print("Worked!");
+   }
+
+   public boolean readText(String text)
+   {
+      if (text.length() < image.MAX_WIDTH)
+      {
+         this.text = text;
+         return true;
+      }
+      return false;
+   }
+
+   public boolean scan(BarcodeImage image)
+   {
+      //this.image = super.clone(image);
+      this.actualWidth = 0;
+      this.actualHeight = 0;
+      cleanImage();
+      return true;
+   }
+
+   public int getActualWidth()
+   {
+      return actualWidth;
+   }
+
+   public int getActualHeight()
+   {
+      return actualHeight;
+   }
+
+   private int computeSignalWidth()
+   {
+      return 1;
+   }
+
+   private int computeSignalHeight()
+   {
+       return 1;
+   }
+
+   public boolean generateImageFromText()
+   {
+      return true;
+   }
+
+   public boolean translateImageToText()
+   {
+      return true;
+   }
+
+   public void displayTextToConsole()
+   {
+      // Something here
+   }
+
+   public void displayImageToConsole()
+   {
+      // Something here
+   }
+
+   private void cleanImage()
+   {
+      // Something here
+   }
+
+   private void moveImageToLowerLeft()
+   {
+      // Something here
+   }
+
+   private void shiftImageDown(int offset)
+   {
+      // Something here
+   }
+   private void shiftImageLeft(int offset)
+   {
+      // Something here
+   }
+
+   private char readCharFromCol(int col)
+   {
+      return 'c';
+   }
+
+   private boolean WriteCharToCol(int col, int code)
+   {
+      return true;
+   }
+
+   public void displayRawImage()
+   {
+      // Something here
+   }
+
+   private void clearImage()
+   {
+      // Something here
+   }
+
+}
